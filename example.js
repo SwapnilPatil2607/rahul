205e18cabdaa8feb646bdc8a18c5676d319d4790
Rahul Ravindran <rahulr1116@gmail.com>
2020-08-16 19:01:46 +0530

    Updated with comments

--- example.js
+++ example.js
@@ -1,10 +1,14 @@
 const runProgram = (input) => {
     let users = [];
     input.split('\r\n').forEach((element) => {
-        processedInput = element.split(' ');
+        // Splits the output with every newLine and forEach element in the array
+        // It further splits with a space For eg: 'rahul 22' will be ['rahul', '22']
+        let processedInput = element.split(' ');
         let newPerson = {
             name: processedInput[0],
             age: +processedInput[1].replace('\r', ''),
+            // In the above line we assign the values properly and check if the age contains '\r' if yes we remove it
+            // and then convert the String into a Number format by adding '+' in the beginning
         };
         users.push(newPerson);
     });
